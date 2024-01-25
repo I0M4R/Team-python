8f57cbe0c7e57e5e0e074582d28ae96e273cbad8
I0M4R <iomar.nasser.suliman55@gmail.com>
2024-01-25 12:01:02 +0200

    solve assignment 19-20

---
+++ assingments/Omar/assignments19-20.py
@@ -0,0 +1,24 @@
+
+
+com = 1+2j
+
+print(com.imag)
+print(com.real)
+
+
+num = 10
+
+print(format(float(num), '.10f'))
+
+
+num = 159.650
+
+print(int(num))
+print(type(int(num)))
+
+
+print(100-115)
+print(50*30)
+print(21%4)
+print(int(110/11))
+print(97//20)
