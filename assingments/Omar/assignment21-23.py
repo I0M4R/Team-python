8c2e59078876f8e41b67a17bdb5e32375c5a34ee
I0M4R <iomar.nasser.suliman55@gmail.com>
2024-01-27 01:26:34 +0200

    fix

--- assingments/Omar/assignment21-23.py
+++ assingments/Omar/assignment21-23.py
@@ -15,6 +15,7 @@ print(friends[1::2])
 friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
 
 print(friends[1:-1])
+print(friends[1:-1])
 print(friends[3:])
 # ---------------------------------------------------4
 friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
