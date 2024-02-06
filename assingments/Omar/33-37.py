fcff573f3685c76ee945aab4a32e817868e415b7
I0M4R <iomar.nasser.suliman55@gmail.com>
2024-02-06 19:04:34 +0200

    assgin 33-37 done

---
+++ assingments/Omar/33-37.py
@@ -0,0 +1,40 @@
+# ---------------------------------------------------1
+print("=" * 40)
+
+print(bool("omar"))
+print(bool(1))
+print(bool(True))
+print(bool(1.1))
+print(bool({}))
+print(bool(""))
+print(bool(False))
+print(bool(()))
+# ---------------------------------------------------2
+print("=" * 40)
+
+html = 80
+css = 60
+javascript = 70
+
+print(html > 50 and css > 50 and javascript > 50)
+# ---------------------------------------------------3
+print("=" * 40)
+
+num_one = 10
+num_two = 20
+num = 20
+
+print(num > num_one or num_two)
+print(num > num_one + num_two)
+# ---------------------------------------------------4
+print("=" * 40)
+
+num_one = 10
+num_two = 20
+result = num_one + num_two
+
+print(result)
+print(result**3)
+print(27000%26000)
+print(1000/5)
+print(type(str(200.0)))
